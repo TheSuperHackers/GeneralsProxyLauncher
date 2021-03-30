595d6cae55ecb625e7752a7479a83d35b0e13871
xezon <4720891+xezon@users.noreply.github.com>
2021-03-30 14:08:25 +0200

    Update MAKE_Release.bat

--- Project/MAKE_Release.bat
+++ Project/MAKE_Release.bat
@@ -1,14 +1,16 @@
+set ArchiveName=GeneralsProxyLauncher_v2.0
 
-set ArchiveName=GeneralsProxyLauncher_v1.9
 set ReleaseDir=Release
 set ReleaseUnpackedDir=ReleaseUnpacked
 set ReleaseUnpackedLauncherDir=%ReleaseUnpackedDir%\launcher
+set ReleaseUnpackedLauncherShellDir=%ReleaseUnpackedDir%\launcherShell
 
 if not exist %ReleaseDir% mkdir %ReleaseDir%
-if not exist %ReleaseUnpackedDir% mkdir %ReleaseUnpackedDir%
 if not exist %ReleaseUnpackedLauncherDir% mkdir %ReleaseUnpackedLauncherDir%
+if not exist %ReleaseUnpackedLauncherShellDir% mkdir %ReleaseUnpackedLauncherShellDir%
 
 xcopy /Y Code\vc9\Release\generals.exe %ReleaseUnpackedLauncherDir%\
+xcopy /Y Code\vc9\Release_ShellExecute\generals.exe %ReleaseUnpackedLauncherShellDir%\
 
 tar.exe -a -c -C %ReleaseUnpackedDir% -f %ReleaseDir%\%ArchiveName%.zip *.*
 
