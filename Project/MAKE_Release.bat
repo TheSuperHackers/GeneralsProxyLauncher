4a55375c308df4a1d6e8b20fa470b18edff30599
xezon <4720891+xezon@users.noreply.github.com>
2024-03-30 17:20:12 +0100

    Remove the separate ShellExecute version of the proxy launcher

--- Project/MAKE_Release.bat
+++ Project/MAKE_Release.bat
@@ -3,13 +3,10 @@ set ArchiveName=GeneralsProxyLauncher_v2.1
 set ReleaseDir=Release
 set ReleaseUnpackedDir=ReleaseUnpacked
 set ReleaseUnpackedLauncherDir=%ReleaseUnpackedDir%\launcher
-set ReleaseUnpackedLauncherShellDir=%ReleaseUnpackedDir%\launcherShell
 
 if not exist %ReleaseDir% mkdir %ReleaseDir%
 if not exist %ReleaseUnpackedLauncherDir% mkdir %ReleaseUnpackedLauncherDir%
-if not exist %ReleaseUnpackedLauncherShellDir% mkdir %ReleaseUnpackedLauncherShellDir%
 
 xcopy /Y Code\vc9\Release\*.exe %ReleaseUnpackedLauncherDir%\
-xcopy /Y Code\vc9\Release_ShellExecute\*.exe %ReleaseUnpackedLauncherShellDir%\
 
 tar.exe -a -c -C %ReleaseUnpackedDir% -f %ReleaseDir%\%ArchiveName%.zip *.*
